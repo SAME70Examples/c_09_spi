d0a96a00d66a8e3eea6eadedb22998da277cf18b
Manuel Limones <limonesu.me@gmail.com>
2015-12-28 09:20:47 -0600

    Add RXT kernel to project and led blinking thread

--- source/main.c
+++ source/main.c
@@ -4,7 +4,33 @@
 #include "usart_same70.h"
 #include "watchdogs.h"
 #include "serial_stdio.h"
-#include <stdlib.h>
+#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
+
+/*----------------------------------------------------------------------------
+ *      Thread 1 'Thread_Name': Sample thread
+ *---------------------------------------------------------------------------*/
+ 
+void Thread1 (void const *argument);                             // thread function
+osThreadId tid_Thread1;                                          // thread id
+osThreadDef (Thread1, osPriorityNormal, 1, 0);                   // thread object
+
+int Thread1_init(void){
+  tid_Thread1 = osThreadCreate (osThread(Thread1), NULL);
+  if(!tid_Thread1) return(-1);
+  
+  return(0);
+}
+
+void Thread1(void const *argument) {
+  while(1){
+		//TODO
+		led_setState(LED_ON);
+    osDelay(500);
+		led_setState(LED_OFF);
+		osDelay(500);
+  }
+}
+
 
 int rx_not_complete = 1;
 void rx_callback(int nCharRead){
@@ -29,12 +55,20 @@ void simple_gets(char* pData){
 }
 
 int main(){
+	//PreKernelConfigration
 	SystemCoreClockUpdate();
 	watchdogs_disable_all();
+	//Initialize kernel
+	osKernelInitialize();
+	//Initialized hardware
 	usart1_init(9600);
 	button_init();
 	led_init();
-
+	//Initialize os objects
+	Thread1_init();
+	//Start kernel and thread switching
+	osKernelStart();
+	//User application
 	char myBuffer[80];
 	int lineCounter = 1;
 
