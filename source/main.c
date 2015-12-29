f5109f17604edfad6ef93a71733ceef799299ddb
Manuel Limones <limonesu.me@gmail.com>
2015-12-29 04:36:56 -0600

    print in two threads

--- source/main.c
+++ source/main.c
@@ -1,14 +1,13 @@
 #include "same70.h"                     // Device header
 #include "board.h"
 #include "delay.h"
-#include "usart_same70.h"
 #include "watchdogs.h"
 #include "serial_stdio.h"
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 #include "os_usart_same70.h"
 
 /*----------------------------------------------------------------------------
- *      Thread 1 'Thread_Name': Sample thread
+ *      Thread 1 'Thread_Name': Led blinker
  *---------------------------------------------------------------------------*/
 
 void Thread1 (void const *argument);                             // thread function
@@ -33,7 +32,7 @@ void Thread1(void const *argument) {
 }
 
 /*----------------------------------------------------------------------------
- *      Thread 2 'Thread_Name': Sample thread
+ *      Thread 2 'Thread_Name': Counter
  *---------------------------------------------------------------------------*/
 
 void Thread2 (void const *argument);                             // thread function
@@ -60,27 +59,29 @@ void Thread2(void const *argument) {
 	}
 }
 
+/*----------------------------------------------------------------------------
+ *      Thread 3 'Thread_Name': Count Printer
+ *---------------------------------------------------------------------------*/
 
-int rx_not_complete = 1;
-void rx_callback(int nCharRead){
-	rx_not_complete = 0;
+void Thread3 (void const *argument);                             // thread function
+osThreadId tid_Thread3;                                          // thread id
+osThreadDef (Thread3, osPriorityNormal, 1, 0);                   // thread object
+
+int Thread3_init(void){
+  tid_Thread3 = osThreadCreate (osThread(Thread3), NULL);
+  if(!tid_Thread3) return(-1);
+
+  return(0);
 }
 
-int tx_not_complete = 1;
-void tx_callback(void){
-	tx_not_complete = 0;
-}
-
-void simple_puts(const char* pData){
-	tx_not_complete = 1;
-	usart1_async_puts(pData,tx_callback);
-	while(tx_not_complete);
-}
-
-void simple_gets(char* pData){
-	rx_not_complete = 1;
-	usart1_async_gets(pData, rx_callback);
-	while(rx_not_complete);
+void Thread3(void const *argument) {
+	thread2_reset_counter = 1;
+	osDelay(1);
+	while(1){
+		serial_printf(os_usart1_puts,"count = %d\n",thread2_counter);
+		thread2_reset_counter = 1;
+		osDelay(1000);
+	}
 }
 
 int main(){
@@ -90,26 +91,24 @@ int main(){
 	//Initialize kernel
 	osKernelInitialize();
 	//Initialized hardware
-	usart1_init(9600);
+	os_usart1_init(9600);
 	button_init();
 	led_init();
 	//Initialize os objects
 	Thread1_init();
 	Thread2_init();
+	Thread3_init();
 	//Start kernel and thread switching
 	osKernelStart();
 	//User application
 	char myBuffer[80];
 	int lineCounter = 1;
 
-	simple_puts("El dinero es dinero\n");
+	os_usart1_puts("El dinero es dinero\n");
 
 	while(1){
-		serial_printf(os_usart1_puts,"%d$",lineCounter);
 		os_usart1_gets(myBuffer);
 		serial_printf(os_usart1_puts,">>%s\n",myBuffer);
-		serial_printf(os_usart1_puts,"count = %d\n",thread2_counter);
-		thread2_reset_counter = 1;
 		lineCounter++;
 	}
 }
