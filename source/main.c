9989a6958832d2aacc6dea2d07c5e0ee32b1aa3a
Manuel Limones <limonesu.me@gmail.com>
2015-12-29 04:22:48 -0600

    use os_usart in main

--- source/main.c
+++ source/main.c
@@ -5,11 +5,12 @@
 #include "watchdogs.h"
 #include "serial_stdio.h"
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
+#include "os_usart_same70.h"
 
 /*----------------------------------------------------------------------------
  *      Thread 1 'Thread_Name': Sample thread
  *---------------------------------------------------------------------------*/
- 
+
 void Thread1 (void const *argument);                             // thread function
 osThreadId tid_Thread1;                                          // thread id
 osThreadDef (Thread1, osPriorityNormal, 1, 0);                   // thread object
@@ -17,18 +18,46 @@ osThreadDef (Thread1, osPriorityNormal, 1, 0);                   // thread objec
 int Thread1_init(void){
   tid_Thread1 = osThreadCreate (osThread(Thread1), NULL);
   if(!tid_Thread1) return(-1);
-  
+
   return(0);
 }
 
 void Thread1(void const *argument) {
   while(1){
-		//TODO
-		led_setState(LED_ON);
-    osDelay(500);
-		led_setState(LED_OFF);
-		osDelay(500);
+	//TODO
+	led_setState(LED_ON);
+	osDelay(500);
+	led_setState(LED_OFF);
+	osDelay(500);
   }
+}
+
+/*----------------------------------------------------------------------------
+ *      Thread 2 'Thread_Name': Sample thread
+ *---------------------------------------------------------------------------*/
+
+void Thread2 (void const *argument);                             // thread function
+osThreadId tid_Thread2;                                          // thread id
+osThreadDef (Thread2, osPriorityNormal, 1, 0);                   // thread object
+
+int Thread2_init(void){
+  tid_Thread2 = osThreadCreate (osThread(Thread2), NULL);
+  if(!tid_Thread2) return(-1);
+
+  return(0);
+}
+
+int thread2_counter;
+int thread2_reset_counter;
+void Thread2(void const *argument) {
+	while(1){
+		if(thread2_reset_counter){
+			thread2_counter = 0;
+			thread2_reset_counter = 0;
+		}else{
+			thread2_counter++;
+		}
+	}
 }
 
 
@@ -66,6 +95,7 @@ int main(){
 	led_init();
 	//Initialize os objects
 	Thread1_init();
+	Thread2_init();
 	//Start kernel and thread switching
 	osKernelStart();
 	//User application
@@ -73,10 +103,13 @@ int main(){
 	int lineCounter = 1;
 
 	simple_puts("El dinero es dinero\n");
-	
+
 	while(1){
-		simple_gets(myBuffer);
-		serial_printf(simple_puts,"%d$%s\n",lineCounter,myBuffer);
+		serial_printf(os_usart1_puts,"%d$",lineCounter);
+		os_usart1_gets(myBuffer);
+		serial_printf(os_usart1_puts,">>%s\n",myBuffer);
+		serial_printf(os_usart1_puts,"count = %d\n",thread2_counter);
+		thread2_reset_counter = 1;
 		lineCounter++;
 	}
 }
